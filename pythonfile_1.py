cf75dbd4113120ce0e42093106dadb3391f404dc
PrakharDutt <prakhar.dutt@bizacuity.com>
2023-03-23 16:11:30 +0530

    Test_2
    Updating and checking wether the pull is working or not

--- pythonfile_1.py
+++ pythonfile_1.py
@@ -1,18 +1,25 @@
 #  code for checking wether a number is prime or not 
 
-input_a=int(input("enter the number 1 \n"))
-count=1
-
-for i in range (2,input_a):
-    if(input_a==1):
-        print(f"{input_a } is neither a prime nor a composite number")
-    elif(input_a%i==0):
-        count=count+1
+def func_prime_or_not (input_a):
+    count=1
+    for i in range (2,input_a):
+        if(input_a==1):
+            print(f"{input_a } is neither a prime nor a composite number")
+        elif(input_a%i==0):
+            count=count+1
         # print(i)
     else:
         count=count+0
 
-if(count==1 and input_a!=1):
-    print(f"the {input_a } is a prime number ")
-else:
-    print(f"the {input_a } is a not a prime number .i.e. is a composite number  ")
+    if(count==1 and input_a!=1):
+        return(print(f"the {input_a } is a prime number "))
+    else:
+        return(print(f"the {input_a } is a not a prime number .i.e. is a composite number  "))
+    
+
+
+
+input_a=int(input("enter the number 1 \n"))
+func_prime_or_not(input_a)
+
+
